3ead239c7711895298193e2153f89828dce078a4
Matthias Bartelmeß <mba@fourplusone.de>
2019-04-28 18:08:21 +0200

    group test

---
+++ jira/provider_test.go
@@ -0,0 +1,43 @@
+package jira
+
+import (
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+var testAccProviders map[string]terraform.ResourceProvider
+var testAccProvider *schema.Provider
+
+func init() {
+	testAccProvider = Provider()
+	testAccProviders = map[string]terraform.ResourceProvider{
+		"jira": testAccProvider,
+	}
+}
+
+func TestProvider(t *testing.T) {
+	if err := Provider().InternalValidate(); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+}
+
+func TestProvider_impl(t *testing.T) {
+	var _ terraform.ResourceProvider = Provider()
+}
+
+func testAccPreCheck(t *testing.T) {
+	if v := os.Getenv("JIRA_URL"); v == "" {
+		t.Fatal("JIRA_URL must be set for acceptance tests")
+	}
+
+	if v := os.Getenv("JIRA_USER"); v == "" {
+		t.Fatal("JIRA_USER must be set for acceptance tests")
+	}
+
+	if v := os.Getenv("JIRA_PASSWORD"); v == "" {
+		t.Fatal("JIRA_PASSWORD must be set for acceptance tests")
+	}
+}
---
+++ jira/resource_group_test.go
@@ -0,0 +1,79 @@
+package jira
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func TestAccJiraGroup_basic(t *testing.T) {
+	// var group gitlab.Group
+	rInt := acctest.RandInt()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckJiraGroupDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccJiraGroupConfig(rInt),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckJiraGroupExists("jira_group.foo"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckJiraGroupDestroy(s *terraform.State) error {
+	client := testAccProvider.Meta().(*Config).jiraClient
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "jira_group" {
+			continue
+		}
+		id := rs.Primary.ID
+
+		_, resp, _ := client.Group.Get(id)
+
+		if resp.StatusCode != 404 {
+			return fmt.Errorf("Group %q still exists", rs.Primary.ID)
+		}
+		return nil
+	}
+	return nil
+}
+
+func testAccCheckJiraGroupExists(n string) resource.TestCheckFunc {
+
+	return func(s *terraform.State) error {
+
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not Found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No group ID is set")
+		}
+
+		client := testAccProvider.Meta().(*Config).jiraClient
+		_, resp, _ := client.Group.Get(rs.Primary.ID)
+
+		if resp.StatusCode != 200 {
+			return fmt.Errorf("Group %q does not exists", rs.Primary.ID)
+		}
+		return nil
+	}
+
+}
+
+func testAccJiraGroupConfig(rInt int) string {
+	return fmt.Sprintf(`
+resource "jira_group" "foo" {
+  name = "foo-name-%d"
+}`, rInt)
+}
