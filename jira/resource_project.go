ac0186d390f4e47dc546e798224465581b25234d
Daniel Noland <daniel@bestateless.com>
2021-01-23 16:35:06 -0700

    Upgrade to Terraform SDK provider 2.4.1
    Simple bump to neweset Terraform SDK provider.
    This was a major SDK version bump but only minor changes were required.
    Specifically, the SDK provider [removed the `helper/hashcode`
    package](https://www.terraform.io/docs/extend/guides/v2-upgrade-guide.html#removal-of-helper-hashcode-package)
    Based on my desire for MVP changes to accomplish this upgrade I went
    along with the advice detailed in the above link and simply used
    trivially modified versions of the public domain functions provided in
    that link.
    
    As far as I can tell, this project does not strictly require the hash
    function to behave exactly this way.
    However, the minimal change should make bisection easier if the version
    bump causes regressions.

--- jira/resource_project.go
+++ jira/resource_project.go
@@ -5,7 +5,7 @@ import (
 	"strconv"
 
 	jira "github.com/andygrunwald/go-jira"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/pkg/errors"
 )
 
--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"github.com/fourplusone/terraform-provider-jira/jira"
-	"github.com/hashicorp/terraform-plugin-sdk/plugin"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: func() terraform.ResourceProvider {
+		ProviderFunc: func() *schema.Provider {
 			return jira.Provider()
 		},
 	})
