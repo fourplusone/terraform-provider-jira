98c70a3643973e20d151b42a5fe43b10d5f5c4e7
hh-nharward <72781511+hh-nharward@users.noreply.github.com>
2021-05-17 13:38:47 -0700

    #19, #20 support for labels and custom fields in issues (#37)
    * #19, #20 - support for using fields and labels in issues. Also added field as a data source so the ID can be looked up by name
    Co-authored-by: Matthias Bartelmeß <mba@fourplusone.de>

--- examples/main.tf
+++ examples/main.tf
@@ -1,12 +1,46 @@
-resource "jira_issue" "example" {
-  assignee    = "anubhavmishra"
-  reporter    = "anubhavmishra"
+// This example works for JIRA projects that support epics
+data "jira_field" "epic_name" {
+  name = "Epic Name"
+}
 
-  issue_type  = "Task"
+data "jira_field" "epic_link" {
+  name = "Epic Link"
+}
 
-  // description is optional  
-  description = "This is a test issue"
-  summary     = "Created using Terraform"
+resource "jira_issue" "example_epic" {
+  assignee = "anubhavmishra"
+  reporter = "anubhavmishra"
+
+  issue_type = "Epic"
+
+  // description is optional
+  description = "This is an epic description"
+  summary     = "This is an epic summary"
+
+  labels = ["one", "two", "buckle-my-shoe"]
+
+  // System and custom fields are optional; see the field data source to reference internal JIRA field IDs by name
+  fields = {
+    (data.jira_field.epic_name.id) = "Example epic name"
+  }
 
   project_key = "PROJ"
 }
+
+resource "jira_issue" "example" {
+  assignee = "anubhavmishra"
+  reporter = "anubhavmishra"
+
+  issue_type = "Task"
+
+  // description is optional
+  description = "This is a test issue that's part of an epic"
+  summary     = "Created using Terraform"
+  labels      = ["label1", "label2"]
+  fields      = {
+    (jira_field.epic_link.id) = jira_issue.example_epic.issue_key
+  }
+
+  project_key = "PROJ"
+}
+
