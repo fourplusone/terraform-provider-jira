5bc29b51ea3b6ac70ed0f9f685ce8d7536101bd5
Sébastien Pierre <spi@dfakto.com>
2024-02-21 14:07:09 +0100

    Use accountId instead of deprecated username to handle group membership

--- jira/resource_group_membership.go
+++ jira/resource_group_membership.go
@@ -10,7 +10,12 @@ import (
 	"github.com/pkg/errors"
 )
 
-// Group The struct sent to the JIRA instance to create a new GroupMembership
+// GroupMembership The struct sent to the JIRA instance to create a new GroupMembership
+type GroupMembership struct {
+	AccountId string `json:"name,omitempty" structs:"name,omitempty"`
+}
+
+// Group
 type Group struct {
 	Name string `json:"name,omitempty" structs:"name,omitempty"`
 }
@@ -25,11 +30,11 @@ type UserGroups struct {
 	Groups Groups `json:"groups,omitempty" structs:"groups,omitempty"`
 }
 
-func getGroups(jiraClient *jira.Client, username string) (*UserGroups, *jira.Response, error) {
+func getGroups(jiraClient *jira.Client, accountId string) (*UserGroups, *jira.Response, error) {
 
 	relativeURL, _ := url.Parse("/rest/api/2/user")
 	query := relativeURL.Query()
-	query.Set("username", username)
+	query.Set("accountId", accountId)
 	query.Set("expand", "groups")
 
 	relativeURL.RawQuery = query.Encode()
@@ -57,7 +62,7 @@ func resourceGroupMembership() *schema.Resource {
 			State: schema.ImportStatePassthrough,
 		},
 		Schema: map[string]*schema.Schema{
-			"username": &schema.Schema{
+			"account_id": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
 				ForceNew: true,
@@ -75,11 +80,11 @@ func resourceGroupMembership() *schema.Resource {
 func resourceGroupMembershipCreate(d *schema.ResourceData, m interface{}) error {
 	config := m.(*Config)
 
-	username := d.Get("username").(string)
+	account_id := d.Get("account_id").(string)
 	group := d.Get("group").(string)
 
-	groupMembership := new(Group)
-	groupMembership.Name = username
+	groupMembership := new(GroupMembership)
+	groupMembership.AccountId = account_id
 
 	relativeURL, _ := url.Parse(groupUserAPIEndpoint)
 	query := relativeURL.Query()
@@ -91,7 +96,7 @@ func resourceGroupMembershipCreate(d *schema.ResourceData, m interface{}) error 
 		return errors.Wrap(err, "Request failed")
 	}
 
-	d.SetId(fmt.Sprintf("%s:%s", username, group))
+	d.SetId(fmt.Sprintf("%s:%s", account_id, group))
 
 	return resourceGroupMembershipRead(d, m)
 }
@@ -101,15 +106,15 @@ func resourceGroupMembershipRead(d *schema.ResourceData, m interface{}) error {
 	config := m.(*Config)
 
 	components := strings.SplitN(d.Id(), ":", 2)
-	username := components[0]
+	accountId := components[0]
 	groupname := components[1]
 
-	groups, _, err := getGroups(config.jiraClient, username)
+	groups, _, err := getGroups(config.jiraClient, accountId)
 	if err != nil {
 		return errors.Wrap(err, "getting jira group failed")
 	}
 
-	d.Set("username", username)
+	d.Set("account_id", accountId)
 	d.Set("group", groupname)
 
 	for _, group := range groups.Groups.Items {
@@ -128,7 +133,7 @@ func resourceGroupMembershipDelete(d *schema.ResourceData, m interface{}) error 
 	relativeURL, _ := url.Parse(groupUserAPIEndpoint)
 
 	query := relativeURL.Query()
-	query.Set("username", d.Get("username").(string))
+	query.Set("accountId", d.Get("account_id").(string))
 	query.Set("groupname", d.Get("group").(string))
 
 	relativeURL.RawQuery = query.Encode()
